c197d602de7b88abe6930317518a0a34a6460a6a
Tammy <tammy.n.tran@gmail.com>
2021-01-19 17:35:14 -0500

    added the following components to the appointment component: confirm, status, error

--- stories/index.js
+++ stories/index.js
@@ -15,6 +15,9 @@ import Appointment from "components/Appointment/index";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
+import Status from 'components/Appointment/Status';
+import Error from 'components/Appointment/Error';
 
 
 // Testing for Button Component
@@ -154,3 +157,9 @@ storiesOf("Appointment", module)
     interviewer={interviewers[0]} 
     onEdit={action("onEdit")}
     onDelete={action('onDelete')} />)
+  .add("Confirm", () => <Confirm 
+    message='Delete the appointment?' 
+    onConfirm={action('onConfirm')}
+    onCancel={action('onCancel')}/>)
+  .add("Status", () => <Status message='Deleting'/>)
+  .add("Error", () => <Error message='Could not delete appointment' onClose={action('onClose')} />)
