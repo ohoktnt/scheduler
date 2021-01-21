3d93801edf7b90266dba1abe8d325834e8e7adb8
Tammy <tammy.n.tran@gmail.com>
2021-01-21 18:22:00 -0500

    refractored selector functions and fixed the getInteriewersForDay function to look into the right database

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,24 +1,19 @@
 // helper functions to restructure data 
 
 export function getAppointmentsForDay(state, day) {
-  // returns an array of appointsments for the day
-  let results = [];
 
   // 1. finding the object in our state.days whos name matches day
-  for (let dayOfArr of state.days) {
-    if (dayOfArr.name === day ) {
-      const appointmentArray = [...dayOfArr.appointments];
-      // 2. access to appt array for the given day, we need to iterate through 
-      for(let appointment of appointmentArray) {
-        // and compare where its id matches the id of states.appointments and return that
-        if(state.appointments[appointment]) {
-          results.push(state.appointments[appointment])
-        }
-      }
-    }
+  const daySelected = state.days.find(eachDay => eachDay.name === day);
+
+  // 2. validation - day does not exist, return []
+  if(!daySelected) {
+    return [];
   }
-  // 3. validation - if no appointments on the given day, return empty []
-  return results;
+
+  // 3. for each appt id in the array, use map to find the appt obj using the id as a key
+  const appointments = daySelected.appointments.map(apptId => state.appointments[apptId]);
+      
+  return appointments;
 }
 
 export function getInterview(state, interview) {
@@ -28,45 +23,102 @@ export function getInterview(state, interview) {
     return null;
   }
 
-  // using the interviewers id find match in the interviewers list
-  for (let interviewer in state.interviewers) {
-    if (interview.interviewer === state.interviewers[interviewer].id) {
-      const interviewerInfo = state.interviewers[interviewer]
-      //construct the result object
-      const result = {
-        'interviewer': {...interviewerInfo},
-        'student': interview.student
-      }
-      // if there is match, no need to complete loop
-      return result;
-    }
+  // convert the interviewers obj into an array
+  const interviewersArr = Object.values(state.interviewers)
+  // use find method to find the interviewer object where id matches the interview info
+  const interviewerInfo = interviewersArr.find(eachInteriviewer => eachInteriviewer.id === interview.interviewer)
+
+  const result = {
+    'interviewer': {...interviewerInfo},
+    'student': interview.student
   }
+  return result;
 }
 
 export function getInterviewersForDay(state, day) {
-  const interviewersId = []
-  const results = []
+  // 1. finding the object in our state.days whos name matches day
+  const daySelected = state.days.find(eachDay => eachDay.name === day);
 
-  // copied from the getAppointmentsForDay function
-  let appointments = getAppointmentsForDay(state, day)
-  // console.log(appointments)
-
-  // iterate through the appointments and get the interviewer, 
-  for (let appointment of appointments) {
-    if(appointment.interview) {
-      interviewersId.push(appointment.interview.interviewer);
-    }
+  // 2. validation - day does not exist, return []
+  if(!daySelected) {
+    return [];
   }
 
-  // iterate through the interviers list and get the interviewer obj
-  for(let interviewer in state.interviewers){
-    // console.log(interviewer)
-    if(interviewersId.includes(state.interviewers[interviewer].id)) {
-      results.push(state.interviewers[interviewer])
-    }
-  }
-  // console.log(interviewersId)
-  // console.log(results)
+  // 3. for each interviewer id in the array, use map to find the interviewer obj using the id as a key
+  const interviewers = daySelected.interviewers.map(interviewerId => state.interviewers[interviewerId]);
 
-  return results;
+  return interviewers;
 }
+
+
+// refractored - getInterviewersFor day was also incorrect
+// export function getAppointmentsForDay(state, day) {
+//   // returns an array of appointsments for the day
+//   let results = [];
+
+//   // 1. finding the object in our state.days whos name matches day
+//   for (let dayOfArr of state.days) {
+//     if (dayOfArr.name === day ) {
+//       const appointmentArray = [...dayOfArr.appointments];
+//       // 2. access to appt array for the given day, we need to iterate through 
+//       for(let appointment of appointmentArray) {
+//         // and compare where its id matches the id of states.appointments and return that
+//         if(state.appointments[appointment]) {
+//           results.push(state.appointments[appointment])
+//         }
+//       }
+//     }
+//   }
+//   // 3. validation - if no appointments on the given day, return empty []
+//   return results;
+// }
+
+// export function getInterview(state, interview) {
+
+//   // if theres is no interview booked
+//   if(interview === null) {
+//     return null;
+//   }
+
+//   // using the interviewers id find match in the interviewers list
+//   for (let interviewer in state.interviewers) {
+//     if (interview.interviewer === state.interviewers[interviewer].id) {
+//       const interviewerInfo = state.interviewers[interviewer]
+//       //construct the result object
+//       const result = {
+//         'interviewer': {...interviewerInfo},
+//         'student': interview.student
+//       }
+//       // if there is match, no need to complete loop
+//       return result;
+//     }
+//   }
+// }
+
+// export function getInterviewersForDay(state, day) {
+//   const interviewersId = []
+//   const results = []
+
+//   // copied from the getAppointmentsForDay function
+//   let appointments = getAppointmentsForDay(state, day)
+//   // console.log(appointments)
+
+//   // iterate through the appointments and get the interviewer, 
+//   for (let appointment of appointments) {
+//     if(appointment.interview) {
+//       interviewersId.push(appointment.interview.interviewer);
+//     }
+//   }
+
+//   // iterate through the interviers list and get the interviewer obj
+//   for(let interviewer in state.interviewers){
+//     // console.log(interviewer)
+//     if(interviewersId.includes(state.interviewers[interviewer].id)) {
+//       results.push(state.interviewers[interviewer])
+//     }
+//   }
+//   // console.log(interviewersId)
+//   // console.log(results)
+
+//   return results;
+// }
--- src/helpers/selectors.test.js
+++ src/helpers/selectors.test.js
@@ -5,12 +5,14 @@ const state = {
     {
       id: 1,
       name: "Monday",
-      appointments: [1, 2, 3]
+      appointments: [1, 2, 3],
+      interviewers: [1, 2]
     },
     {
       id: 2,
       name: "Tuesday",
-      appointments: [4, 5]
+      appointments: [4, 5],
+      interviewers: [2, 1]
     }
   ],
   appointments: {
@@ -97,7 +99,7 @@ test("getInterviewersForDay returns an array", () => {
 
 test("getInterviewersForDay returns an array with a length matching the number of appointments for that day", () => {
   const result = getInterviewersForDay(state, "Monday");
-  expect(result.length).toEqual(1);
+  expect(result.length).toEqual(2);
 });
 
 test("getInterviewersForDay returns an array containing the correct interviewers objects", () => {
