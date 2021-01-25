34be335e4bfe422d01277e77ea0a56b91060662b
Tammy <tammy.n.tran@gmail.com>
2021-01-25 17:18:06 -0500

    convert initial test to use async and await for application

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -7,13 +7,26 @@ import { fireEvent } from "@testing-library/react/dist";
 
 afterEach(cleanup);
 
-it("defaults to Monday and changes the schedule whena new day is selected", () => {
+// it("defaults to Monday and changes the schedule whena new day is selected", () => {
+//   const { getByText } = render(<Application />);
+
+//   return waitForElement(()=> getByText("Monday"))
+//     .then(() => {
+//       fireEvent.click(getByText('Tuesday'));
+//       expect(getByText('Leopold Silvers')).toBeInTheDocument()
+//     }
+//   )
+
+// });
+
+// using new syntax from ECMAScript 2017
+it("changes the schedule when a new day is selected", async () => {
   const { getByText } = render(<Application />);
 
-  return waitForElement(()=> getByText("Monday"))
-    .then(() => {
-      fireEvent.click(getByText('Tuesday'));
-      expect(getByText('Leopold Silvers')).toBeInTheDocument()
-    }
-  )
-});
+  await waitForElement(() => getByText("Monday"));
+
+  fireEvent.click(getByText('Tuesday'));
+
+  expect(getByText('Leopold Silvers')).toBeInTheDocument()
+  
+})
