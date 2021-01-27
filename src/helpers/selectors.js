fc6329a251958c908e8b6fab06ec77b1e4c5c05f
Tammy <tammy.n.tran@gmail.com>
2021-01-27 17:11:43 -0500

    refractored helper selectors

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,7 +1,6 @@
 // helper functions to restructure data 
 
 export function getAppointmentsForDay(state, day) {
-
   // 1. finding the object in our state.days whos name matches day
   const daySelected = state.days.find(eachDay => eachDay.name === day);
 
@@ -17,22 +16,23 @@ export function getAppointmentsForDay(state, day) {
 }
 
 export function getInterview(state, interview) {
-
-  // if theres is no interview booked
+  // 1. if theres is no interview booked
   if(interview === null) {
     return null;
   }
 
-  // convert the interviewers obj into an array
-  const interviewersArr = Object.values(state.interviewers)
-  // use find method to find the interviewer object where id matches the interview info
-  const interviewerInfo = interviewersArr.find(eachInteriviewer => eachInteriviewer.id === interview.interviewer)
+  // 2. convert the interviewers obj into an array
+  const interviewersArr = Object.values(state.interviewers);
 
-  const result = {
+  // 3. use find method to find the interviewer object where id matches the interview info
+  const interviewerInfo = interviewersArr.find(eachInteriviewer => eachInteriviewer.id === interview.interviewer);
+
+  const bookedInterview = {
     'interviewer': {...interviewerInfo},
     'student': interview.student
-  }
-  return result;
+  };
+
+  return bookedInterview;
 }
 
 export function getInterviewersForDay(state, day) {
@@ -49,76 +49,3 @@ export function getInterviewersForDay(state, day) {
 
   return interviewers;
 }
-
-
-// refractored - getInterviewersFor day was also incorrect
-// export function getAppointmentsForDay(state, day) {
-//   // returns an array of appointsments for the day
-//   let results = [];
-
-//   // 1. finding the object in our state.days whos name matches day
-//   for (let dayOfArr of state.days) {
-//     if (dayOfArr.name === day ) {
-//       const appointmentArray = [...dayOfArr.appointments];
-//       // 2. access to appt array for the given day, we need to iterate through 
-//       for(let appointment of appointmentArray) {
-//         // and compare where its id matches the id of states.appointments and return that
-//         if(state.appointments[appointment]) {
-//           results.push(state.appointments[appointment])
-//         }
-//       }
-//     }
-//   }
-//   // 3. validation - if no appointments on the given day, return empty []
-//   return results;
-// }
-
-// export function getInterview(state, interview) {
-
-//   // if theres is no interview booked
-//   if(interview === null) {
-//     return null;
-//   }
-
-//   // using the interviewers id find match in the interviewers list
-//   for (let interviewer in state.interviewers) {
-//     if (interview.interviewer === state.interviewers[interviewer].id) {
-//       const interviewerInfo = state.interviewers[interviewer]
-//       //construct the result object
-//       const result = {
-//         'interviewer': {...interviewerInfo},
-//         'student': interview.student
-//       }
-//       // if there is match, no need to complete loop
-//       return result;
-//     }
-//   }
-// }
-
-// export function getInterviewersForDay(state, day) {
-//   const interviewersId = []
-//   const results = []
-
-//   // copied from the getAppointmentsForDay function
-//   let appointments = getAppointmentsForDay(state, day)
-//   // console.log(appointments)
-
-//   // iterate through the appointments and get the interviewer, 
-//   for (let appointment of appointments) {
-//     if(appointment.interview) {
-//       interviewersId.push(appointment.interview.interviewer);
-//     }
-//   }
-
-//   // iterate through the interviers list and get the interviewer obj
-//   for(let interviewer in state.interviewers){
-//     // console.log(interviewer)
-//     if(interviewersId.includes(state.interviewers[interviewer].id)) {
-//       results.push(state.interviewers[interviewer])
-//     }
-//   }
-//   // console.log(interviewersId)
-//   // console.log(results)
-
-//   return results;
-// }
